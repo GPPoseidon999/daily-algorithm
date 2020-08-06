ea73ce78e1364c6d90570f2f308e9e0153621978
俞鹏飞 <yupengfei@aifin.ltd>
2020-08-06 18:40:51 +0800

    update code

---
+++ code/stack-object.js
@@ -0,0 +1,52 @@
+class Stack {
+    constructor(){
+        this.count = 0
+        this.items = {}
+    }
+    // 插入一个元素
+    push(element){
+        this.items[this.count] = element
+        this.count++
+    }
+    // 栈的大小
+    size(){
+        return this.count
+    }
+    // 栈是否为空
+    isEmpty(){
+        return  this.count === 0
+    }
+    // 移除栈顶元素
+    pop(){
+        if(this.isEmpty()){
+            return undefined
+        }
+        this.count--
+        const result = this.items[this.count]
+        delete this.items[this.count]
+        return result
+    }
+    // 查看栈顶元素
+    peek(){
+        if(this.isEmpty()){
+            return undefined
+        }
+        return this.items[this.count -1]
+    }
+    // 清空栈
+    clear(){
+        this.items = {}
+        this.count = 0
+    }
+    // 打印栈内容
+    toString(){
+        if(this.isEmpty()){
+            return ''
+        }
+        let objString = `${this.items[0]}`
+        for (let i= 1; i < this.count; i++) {
+            objString = `${objString}, ${this.items[i]}`
+        }
+        return objString
+    }
+}
